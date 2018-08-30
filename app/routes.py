1ce17c37ee8f570d92e49e7d4abcc6bada70e5b1
LiteOne1 <39678787+LiteOne1@users.noreply.github.com>
2018-08-30 17:19:34 +0300

    movie search added v_0

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,13 @@
 
 from flask import render_template, url_for
 from app import app
+from app.movie import get_synopsis, url, get_name
 
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    synopsis = get_synopsis(url)
+    movie_name = get_name(url)
+    return render_template('index.html', synopsis = synopsis, name = movie_name)
