bfb5c37cb6eb9833d5f5b8e221c8b8976ebc2cc5
LiteOne1 <39678787+LiteOne1@users.noreply.github.com>
2018-09-04 16:36:20 +0300

    added trailers. Adding posters still not working.

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 
 from flask import render_template, url_for
 from app import app
-from app.movie import get_synopsis, url, get_name
+from app.movie import get_synopsis, url, get_actors, get_name, url_imdb, imdb_rating, rotten_rating
 
 
 
@@ -10,4 +10,7 @@ from app.movie import get_synopsis, url, get_name
 def index():
     synopsis = get_synopsis(url)
     movie_name = get_name(url)
-    return render_template('index.html', synopsis = synopsis, name = movie_name)
+    actors = get_actors(url)
+    imdb_score = imdb_rating(url_imdb)
+    rotten_score = rotten_rating(url)
+    return render_template('index.html', synopsis = synopsis, name = movie_name, actors = actors, score = imdb_score, rotten_score = rotten_score)
